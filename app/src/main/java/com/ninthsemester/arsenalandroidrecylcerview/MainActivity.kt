a63ea5159033c89018a2dd7f2e369c4b2687248b
Sahil Patel <sahil-mac@Sahils-MacBook-Pro.local>
2018-04-02 22:20:41 +0530

    added some basic code for the scenario. Code not switches between list to grid

--- app/src/main/java/com/ninthsemester/arsenalandroidrecylcerview/MainActivity.kt
+++ app/src/main/java/com/ninthsemester/arsenalandroidrecylcerview/MainActivity.kt
@@ -1,12 +1,104 @@
 package com.ninthsemester.arsenalandroidrecylcerview
 
+import android.graphics.Rect
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.DividerItemDecoration
+import android.support.v7.widget.GridLayoutManager
+import android.support.v7.widget.LinearLayoutManager
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
+import android.support.v7.widget.RecyclerView
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+
 
 class MainActivity : AppCompatActivity() {
+
+    private val spanCount = 2
+
+    private val pirates : Array<String> by lazy { resources.getStringArray(R.array.pirates) }
+    private val ships : Array<String> by lazy { resources.getStringArray(R.array.ships) }
+
+    private val listItemDecorator : DividerItemDecoration by lazy {
+        DividerItemDecoration(this, LinearLayoutManager.VERTICAL)
+    }
+
+    private val gridItemDecorator : GridSpacingItemDecoration by lazy {
+        GridSpacingItemDecoration(spanCount, 25, true)
+    }
+
+    private lateinit var adapter: PiratesAdapter
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setupList()
     }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu,menu)
+        return true
+    }
+
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean =
+            when(item?.itemId) {
+                R.id.action_list_view -> {
+                    setupList(true)
+                    true
+                }
+                R.id.action_grid_view -> {
+                    setupList(false)
+                    true
+                }
+                else -> super.onOptionsItemSelected(item)
+            }
+
+    private fun setupList(showAsList: Boolean = true) {
+
+        adapter = PiratesAdapter(pirates.zip(ships).toMutableList(), showAsList)
+
+        recyclerView.removeItemDecoration(gridItemDecorator)
+        recyclerView.removeItemDecoration(listItemDecorator)
+        if (showAsList){
+            recyclerView.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
+            recyclerView.addItemDecoration(listItemDecorator)
+        }
+        else{
+            recyclerView.layoutManager = GridLayoutManager(this, spanCount)
+            recyclerView.addItemDecoration(gridItemDecorator)
+        }
+
+        recyclerView.adapter = adapter
+    }
+
+
+
+    inner class GridSpacingItemDecoration(private val spanCount: Int, private val spacing: Int, private val includeEdge: Boolean) : RecyclerView.ItemDecoration() {
+
+        override fun getItemOffsets(outRect: Rect, view: View, parent: RecyclerView, state: RecyclerView.State?) {
+            val position = parent.getChildAdapterPosition(view) // item position
+            val column = position % spanCount // item column
+
+            if (includeEdge) {
+                outRect.left = spacing - column * spacing / spanCount // spacing - column * ((1f / spanCount) * spacing)
+                outRect.right = (column + 1) * spacing / spanCount // (column + 1) * ((1f / spanCount) * spacing)
+
+                if (position < spanCount) { // top edge
+                    outRect.top = spacing
+                }
+                outRect.bottom = spacing // item bottom
+            } else {
+                outRect.left = column * spacing / spanCount // column * ((1f / spanCount) * spacing)
+                outRect.right = spacing - (column + 1) * spacing / spanCount // spacing - (column + 1) * ((1f /    spanCount) * spacing)
+                if (position >= spanCount) {
+                    outRect.top = spacing // item top
+                }
+            }
+        }
+    }
+
 }
---
+++ app/src/main/java/com/ninthsemester/arsenalandroidrecylcerview/PiratesAdapter.kt
@@ -0,0 +1,65 @@
+package com.ninthsemester.arsenalandroidrecylcerview
+
+import android.content.res.ColorStateList
+import android.graphics.Color
+import android.graphics.drawable.GradientDrawable
+import android.os.Build
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import java.util.*
+
+/**
+ * Created by sahil-mac on 02/04/18.
+ */
+class PiratesAdapter(private val items : MutableList<Pair<String, String>>,
+                     private val showAsList : Boolean = true) : RecyclerView.Adapter<PiratesAdapter.PirateViewHolder>(){
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PirateViewHolder =
+            when(showAsList) {
+                true -> LayoutInflater.from(parent.context)
+                        .inflate(R.layout.rv_item_list,parent, false)
+                        .run { PirateViewHolder(this) }
+
+                false-> LayoutInflater.from(parent.context)
+                        .inflate(R.layout.rv_item_grid,parent, false)
+                        .run { PirateViewHolder(this) }
+            }
+
+
+    override fun onBindViewHolder(holder: PirateViewHolder, position: Int) = holder.bind(items[position])
+
+    override fun getItemCount() : Int = items.size
+
+
+
+    inner class PirateViewHolder(
+            itemView: View,
+            private val imageView : ImageView = itemView.findViewById(R.id.imageView),
+            private val tvName: TextView = itemView.findViewById(R.id.tvName),
+            private val tvVessel: TextView = itemView.findViewById(R.id.tvVessel)) : RecyclerView.ViewHolder(itemView) {
+
+        fun bind(data : Pair<String, String>) {
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                val drawable = imageView.background as GradientDrawable
+                drawable.color = ColorStateList.valueOf(getRandomColor())
+            }
+
+            tvName.text = data.first
+            tvVessel.text = data.second
+        }
+    }
+
+    companion object {
+
+        fun getRandomColor(): Int =
+            with(Random()) {
+                return Color.argb(255, nextInt(), nextInt(), nextInt())
+            }
+        }
+}
